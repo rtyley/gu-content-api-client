60b47e572b29dee79789a71c1a77cffa9a87d6c7
Roberto Tyley <roberto.tyley@guardian.co.uk>
2010-04-22 17:47:14 +0100

    Sorted the injection of this things deps

--- guardian-guardian/src/main/java/com/madgag/guardian/guardian/content/SingleContentApiRequestBulkNormalisedArticleService.java
+++ guardian-guardian/src/main/java/com/madgag/guardian/guardian/content/SingleContentApiRequestBulkNormalisedArticleService.java
@@ -4,17 +4,20 @@ import static com.google.common.collect.Lists.transform;
 
 import java.util.List;
 
+import com.google.inject.Inject;
 import com.madgag.guardian.contentapi.SearchRequest;
 import com.madgag.guardian.guardian.BulkNormalisedArticleService;
 import com.madgag.guardian.guardian.ContentNormaliserTransform;
+import com.madgag.guardian.guardian.PopulatedArticleSearchRequestProvider;
 import com.madgag.guardian.guardian.spom.detection.NormalisedArticle;
 
 final class SingleContentApiRequestBulkNormalisedArticleService implements BulkNormalisedArticleService {
 	
 	private final SearchRequest req;
 
-	SingleContentApiRequestBulkNormalisedArticleService(SearchRequest req) {
-		this.req = req;
+	@Inject
+	SingleContentApiRequestBulkNormalisedArticleService(PopulatedArticleSearchRequestProvider pro) {
+		this.req = pro.populatedArticleSearch();
 	}
 
 	public List<NormalisedArticle> apply(List<String> chunkIds) {
